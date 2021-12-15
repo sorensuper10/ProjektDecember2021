251c7bf79c62a59f655a153182c2faf50c53ba68
sorensuper10 <89455430+sorensuper10@users.noreply.github.com>
2021-12-15 14:29:57 +0100

    Add files via upload

---
+++ DbConsoleInputs.java
@@ -0,0 +1,34 @@
+package com.example.trackntrace;
+
+public class DbConsoleInputs {
+
+  /*  create table Costumers (
+    userId integer not null,
+    userFirstName char(20),
+    userLastName char(20),
+    userMail char(20),
+    userPhone char(8),
+    userAdress char(10),
+    userZipcode char(4),
+    userCountry char(10),
+    userNote char(10),
+    primary key(userId)
+);
+
+    create table Packages (
+    packageId integer not null,
+    packageName char(20),
+    packageWeight double(4),
+    packageSendDate char(20),
+    packageTrackingId char(10),
+    primary key(packageId)
+);
+
+create table Placement (
+    placementId integer not null ,
+    placementDescription varchar(10)
+);
+
+*/
+
+}
